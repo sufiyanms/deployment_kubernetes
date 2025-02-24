608cbf8e63591393b372955cac241d5f5cfecb7b
unknown <sufiyanmohammed454@gmail.com>
2025-02-24 15:26:37 +0530

    changes

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+let port=3000;
+
+
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+    res.send("welcome")
+})
+
+app.listen(port,()=>{
+    console.log("server started");
+})
